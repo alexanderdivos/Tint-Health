d0c7284e598a5f200ee95d2a1f9780de6b9992dd
Alexandre Souleau <mrsheepsheepandcie@gmail.com>
2015-05-09 13:29:35 +0200

    Added support for enable and disable

--- src/fr/mrsheepsheep/tinthealth/PlayerListener.java
+++ src/fr/mrsheepsheep/tinthealth/PlayerListener.java
@@ -17,30 +17,35 @@ public class PlayerListener implements Listener {
 	protected PlayerListener(TintHealth plugin){
 		this.plugin = plugin;
 		Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
-		
+
 	}
-	
+
+	protected boolean isEnabled(){
+		return plugin.enabled;
+	}
 	@EventHandler(ignoreCancelled=true, priority=EventPriority.HIGH)
 	protected void onDamage(EntityDamageEvent e){
-		if (e.getEntity() instanceof Player){
-			Player p = (Player) e.getEntity();
-			if (plugin.functions.isTintEnabled(p)){
-				int health = plugin.functions.getPlayerHealth(p);
-				if (health <= plugin.minhearts){
-					int maxhealth = plugin.functions.getMaxPlayerHealth(p);
-					int percentage;
+		if (isEnabled()){
+			if (e.getEntity() instanceof Player){
+				Player p = (Player) e.getEntity();
+				if (plugin.functions.isTintEnabled(p)){
+					int health = plugin.functions.getPlayerHealth(p);
+					if (health <= plugin.minhearts){
+						int maxhealth = plugin.functions.getMaxPlayerHealth(p);
+						int percentage;
 
-					if (plugin.damagemode){
-						health = (int) e.getDamage();
-						percentage = 100 - (health*100) / maxhealth;
-					}
-					else
-					{
-						health = (int) (health - e.getDamage());
-						percentage = (health*100) / maxhealth;
-					}
+						if (plugin.damagemode){
+							health = (int) e.getDamage();
+							percentage = 100 - (health*100) / maxhealth;
+						}
+						else
+						{
+							health = (int) (health - e.getDamage());
+							percentage = (health*100) / maxhealth;
+						}
 
-					plugin.functions.sendBorder(p, percentage);
+						plugin.functions.sendBorder(p, percentage);
+					}
 				}
 			}
 		}
@@ -48,27 +53,10 @@ public class PlayerListener implements Listener {
 
 	@EventHandler(ignoreCancelled=true)
 	protected void onJoin(PlayerJoinEvent e){
-		if (!plugin.fade){
-			final Player p = e.getPlayer();
-			if (plugin.functions.isTintEnabled(p)){
-				final int percentage = plugin.functions.getPlayerHealthPercentage(p);
-				Runnable run = new Runnable() {
-					@Override
-					public void run() {
-						plugin.functions.sendBorder(p, percentage);
-					}
-				};
-				plugin.smallDelay(run);
-			}
-		}
-	}
-
-	@EventHandler(ignoreCancelled=true)
-	protected void onTeleport(PlayerTeleportEvent e){
-		if (!plugin.fade){
-			final Player p = e.getPlayer();
-			if (plugin.functions.isTintEnabled(p)){
-				if (plugin.functions.getPlayerHealth(p) <= plugin.minhearts){
+		if (isEnabled()){
+			if (!plugin.fade){
+				final Player p = e.getPlayer();
+				if (plugin.functions.isTintEnabled(p)){
 					final int percentage = plugin.functions.getPlayerHealthPercentage(p);
 					Runnable run = new Runnable() {
 						@Override
@@ -83,15 +71,38 @@ public class PlayerListener implements Listener {
 	}
 
 	@EventHandler(ignoreCancelled=true)
-	protected void onHeal(EntityRegainHealthEvent e){
-		if (!plugin.fade){
-			if (e.getEntity() instanceof Player){
-				Player p = (Player) e.getEntity();
+	protected void onTeleport(PlayerTeleportEvent e){
+		if (isEnabled()){
+			if (!plugin.fade){
+				final Player p = e.getPlayer();
 				if (plugin.functions.isTintEnabled(p)){
-					Damageable d = (Damageable) p;
-					double heal = e.getAmount();
-					int health = (int) (d.getHealth() + heal);
-					plugin.functions.sendBorder(p, health);
+					if (plugin.functions.getPlayerHealth(p) <= plugin.minhearts){
+						final int percentage = plugin.functions.getPlayerHealthPercentage(p);
+						Runnable run = new Runnable() {
+							@Override
+							public void run() {
+								plugin.functions.sendBorder(p, percentage);
+							}
+						};
+						plugin.smallDelay(run);
+					}
+				}
+			}
+		}
+	}
+
+	@EventHandler(ignoreCancelled=true)
+	protected void onHeal(EntityRegainHealthEvent e){
+		if (isEnabled()){
+			if (!plugin.fade){
+				if (e.getEntity() instanceof Player){
+					Player p = (Player) e.getEntity();
+					if (plugin.functions.isTintEnabled(p)){
+						Damageable d = (Damageable) p;
+						double heal = e.getAmount();
+						int health = (int) (d.getHealth() + heal);
+						plugin.functions.sendBorder(p, health);
+					}
 				}
 			}
 		}
