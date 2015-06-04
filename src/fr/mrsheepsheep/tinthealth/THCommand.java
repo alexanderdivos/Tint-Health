bffecd443ef939a1343400cc8ed42314f4ae406e
mrCookieSlime <mrCookieSlime@gmail.com>
2015-06-04 16:02:45 +0200

    Removal of ProtocolLib as a Dependency; Java 6 Support

--- src/fr/mrsheepsheep/tinthealth/THCommand.java
+++ src/fr/mrsheepsheep/tinthealth/THCommand.java
@@ -9,8 +9,8 @@ import org.bukkit.entity.Player;
 public class THCommand implements CommandExecutor {
 
 	TintHealth plugin;
-	String heart = "\u2764 ";
-	String bloodurl = "http://download1627.mediafire.com/58crbw4351og/wpc5hr8s1lca9ph/BloodScreen.zip";
+	final String heart = "\u2764 ";
+	final String bloodurl = "http://download1627.mediafire.com/58crbw4351og/wpc5hr8s1lca9ph/BloodScreen.zip";
 
 	public THCommand(TintHealth plugin){
 		this.plugin = plugin;
@@ -18,74 +18,65 @@ public class THCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if (args.length == 1){
-			switch (args[0]){
-			case "reload":
-				if (sender.hasPermission("tinthealth.reload")){
+		if (args.length == 1) {
+			if (args[0].equalsIgnoreCase("reload")) {
+				if (sender.hasPermission("tinthealth.reload")) {
 					plugin.reloadConfig();
 					plugin.loadConfig();
 					sender.sendMessage(ChatColor.GREEN + "TintHealth configuration reloaded");
 				}
-				else
-				{
+				else {
 					String prefix = ChatColor.DARK_RED + heart + ChatColor.RED;
 					sender.sendMessage(prefix + "You don't have permission.");
 				}
-				break;
-			case "toggle":
+			}
+			else if (args[0].equalsIgnoreCase("toggle")) {
 				if (sender instanceof Player){
 					String prefix = ChatColor.DARK_RED + heart + ChatColor.RED;
 					Player p = (Player) sender;
 					if (p.hasPermission("tinthealth.toggle")){
 						plugin.functions.togglePlayerTint(p);
-						if (plugin.functions.isTintEnabled(p))
-							p.sendMessage(prefix + "Tint is now " + ChatColor.GREEN + "enabled" + ChatColor.RED + ".");
-						else
-							p.sendMessage(prefix + "Tint is now " + ChatColor.GRAY + "disabled" + ChatColor.RED + ".");
+						if (plugin.functions.isTintEnabled(p)) p.sendMessage(prefix + "Tint is now " + ChatColor.GREEN + "enabled" + ChatColor.RED + ".");
+						else p.sendMessage(prefix + "Tint is now " + ChatColor.GRAY + "disabled" + ChatColor.RED + ".");
 					}
-					else
-						p.sendMessage(prefix + "You don't have permission.");
+					else p.sendMessage(prefix + "You don't have permission.");
 				}
-				else
-					sender.sendMessage("This is an in-game only command !");
-				break;
-			case "blood":
-				if (sender instanceof Player){
+				else sender.sendMessage("This is an in-game only command !");
+			}
+			else if (args[0].equalsIgnoreCase("blood")) {
+				if (sender instanceof Player) {
 					String prefix = ChatColor.DARK_RED + heart + ChatColor.RED;
 					Player p = (Player) sender;
 					if (p.hasPermission("tinthealth.blood")){
 						p.setResourcePack(bloodurl);
 						p.sendMessage(prefix + "Blood texture has been sent. If you did not enable server resource pack, nothing will happen.");
 					}
-					else
-						p.sendMessage("You don't have permission");
+					else p.sendMessage("You don't have permission");
 				}
-				else
-					sender.sendMessage("This is an in-game only command !");
-				break;
-			default:
-				sendHelpMessage(sender);
-				break;
+				else sender.sendMessage("This is an in-game only command !");
 			}
-			return true;
+			else sendHelpMessage(sender);
 		}
-		sendHelpMessage(sender);
+		else sendHelpMessage(sender);
 		return true;
 	}
 
 	public void sendHelpMessage(CommandSender sender){
 		String prefix = "[TintHealth] ";
-		if (sender instanceof Player)
-			prefix = ChatColor.DARK_RED + heart + ChatColor.GRAY;
+		if (sender instanceof Player) prefix = ChatColor.DARK_RED + heart + ChatColor.GRAY;
 
 		sender.sendMessage(prefix + ChatColor.RED + "TintHealth v" + ChatColor.GRAY + plugin.getDescription().getVersion() + ChatColor.RED + " by " + ChatColor.GRAY + "MrSheepSheep " + prefix);
 		sender.sendMessage(prefix + ChatColor.YELLOW + "" + ChatColor.BOLD + "Available commands:");
-		if (sender.hasPermission("tinthealth.reload"))
+		
+		if (sender.hasPermission("tinthealth.reload")) {
 			sender.sendMessage(prefix + "/tint reload" + ChatColor.RED + " - Reloads the config");
-		if (sender.hasPermission("tinthealth.toggle"))
+		}
+		if (sender.hasPermission("tinthealth.toggle")) {
 			sender.sendMessage(prefix + "/tint toggle" + ChatColor.RED + " - Enable / Disable the tint for you");
-		if (!sender.hasPermission("tinthealth.toggle") && !sender.hasPermission("tinthealth.reload"))
+		}
+		if (!sender.hasPermission("tinthealth.toggle") && !sender.hasPermission("tinthealth.reload")) {
 			sender.sendMessage(prefix + "None !");
+		}
 		sender.sendMessage(prefix + "/tint blood" + ChatColor.RED + " - Install a blood texture. Credits goes to Macaron on the Spigot forums !");
 	}
 
--- src/fr/mrsheepsheep/tinthealth/THFunctions.java
+++ src/fr/mrsheepsheep/tinthealth/THFunctions.java
@@ -1,36 +1,76 @@
 package fr.mrsheepsheep.tinthealth;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Player;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.wrappers.EnumWrappers.WorldBorderAction;
-
 public class THFunctions {
+	
+	private static Method handle, sendPacket;
+	private static Method center, distance, time, movement;
+	private static Field player_connection;
+	private static Constructor<?> constructor, border_constructor;
+	private static Object constant;
+	
+	static {
+	    try {
+			handle = getClass("org.bukkit.craftbukkit", "entity.CraftPlayer").getMethod("getHandle");
+			player_connection = getClass("net.minecraft.server", "EntityPlayer").getField("playerConnection");
+			for (Method m : getClass("net.minecraft.server", "PlayerConnection").getMethods()) {
+				if (m.getName().equals("sendPacket")) {
+					sendPacket = m;
+					break;
+				}
+			}
+			Class<?> enumclass;
+			try {
+				enumclass = getClass("net.minecraft.server", "EnumWorldBorderAction");
+			} catch(ClassNotFoundException x) {
+				enumclass = getClass("net.minecraft.server", "PacketPlayOutWorldBorder$EnumWorldBorderAction");
+			}
+			constructor = getClass("net.minecraft.server", "PacketPlayOutWorldBorder").getConstructor(getClass("net.minecraft.server", "WorldBorder"), enumclass);
+			border_constructor = getClass("net.minecraft.server", "WorldBorder").getConstructor();
+			
+			center = getClass("net.minecraft.server", "WorldBorder").getMethod("setCenter", double.class, double.class);
+			distance = getClass("net.minecraft.server", "WorldBorder").getMethod("setWarningDistance", int.class);
+			time = getClass("net.minecraft.server", "WorldBorder").getMethod("setWarningTime", int.class);
+			movement = getClass("net.minecraft.server", "WorldBorder").getMethod("transitionSizeBetween", double.class, double.class, long.class);
+			
+			for (Object o: enumclass.getEnumConstants()) {
+				if (o.toString().equals("INITIALIZE")) {
+					constant = o;
+					break;
+				}
+			}
+	    } catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	private static Class<?> getClass(String prefix, String name) throws Exception {
+		return Class.forName(new StringBuilder().append(prefix + ".").append(Bukkit.getServer().getClass().getPackage().getName().substring(Bukkit.getServer().getClass().getPackage().getName().lastIndexOf(".") + 1)).append(".").append(name).toString());
+	}
+	
 	TintHealth plugin;
-	protected ProtocolManager protocolManager;
 	protected List<String> togglelist = new ArrayList<String>();
 
 	protected THFunctions(TintHealth plugin) {
 		this.plugin = plugin;
-		protocolManager = ProtocolLibrary.getProtocolManager();
 	}
 
 	protected void sendBorder(Player p, int percentage){
 		percentage = Math.round(percentage / plugin.intensity);
 		setBorder(p, percentage);
-		if (plugin.fade && p.hasPermission("tinthealth.fade"))
-			fadeBorder(p, percentage, plugin.fadetime);
+		if (plugin.fade && p.hasPermission("tinthealth.fade")) fadeBorder(p, percentage, plugin.fadetime);
 	}
 
-	protected void fadeBorder(Player p, int percentage, int time){
+	protected void fadeBorder(Player p, int percentage, long time){
 		int dist = -10000 * percentage + 1300000;
 		sendWorldBorderPacket(p, 0, 200000D, (double) dist, (long) 1000 * time + 4000); //Add 4000 to make sure the "security" zone does not count in the fade time
 	}
@@ -44,71 +84,77 @@ public class THFunctions {
 		sendWorldBorderPacket(p, dist, 200000D, 200000D, 0);
 	}
 
-	protected void sendWorldBorderPacket(Player p, int dist, double oldradius, double newradius, long time){
-
-		PacketContainer border = protocolManager.createPacket(PacketType.Play.Server.WORLD_BORDER);
-		border.getWorldBorderActions().write(0, WorldBorderAction.INITIALIZE);
-		border.getIntegers()
-		.write(0, 29999984)
-		.write(1, 15)
-		.write(2, dist);
-		border.getLongs()
-		.write(0, time);
-		border.getDoubles()
-		.write(0, p.getLocation().getX())
-		.write(1, p.getLocation().getY())
-		.write(2, newradius)
-		.write(3, oldradius);
+	protected void sendWorldBorderPacket(Player p, int dist, double oldradius, double newradius, long delay) {
+//		
+//			New Reflection Version
+//		
 		try {
-			protocolManager.sendServerPacket(p, border);
-		} catch (InvocationTargetException e) {
-			throw new RuntimeException(
-					"Cannot send packet " + border, e);
+			Object worldborder = border_constructor.newInstance();
+			center.invoke(worldborder, p.getLocation().getX(), p.getLocation().getY());
+			distance.invoke(worldborder, dist);
+			time.invoke(worldborder, 15);
+			movement.invoke(worldborder, oldradius, newradius, delay);
+			
+			Object packet = constructor.newInstance(worldborder, constant);
+			sendPacket.invoke(player_connection.get(handle.invoke(p)), packet);
+		} catch(Exception x) {
+			x.printStackTrace();
 		}
+		
+//			Old ProtocollLib Version:
+//		
+//		border.getWorldBorderActions().write(0, WorldBorderAction.INITIALIZE);
+//		border.getIntegers()
+//		.write(0, 29999984)
+//		.write(1, 15)
+//		.write(2, dist);
+//		border.getLongs()
+//		.write(0, time);
+//		border.getDoubles()
+//		.write(0, p.getLocation().getX())
+//		.write(1, p.getLocation().getY())
+//		.write(2, newradius)
+//		.write(3, oldradius);
+//		try {
+//			protocolManager.sendServerPacket(p, border);
+//		} catch (InvocationTargetException e) {
+//			throw new RuntimeException("Cannot send packet " + border, e);
+//		}
 	}
 
 	protected int getPlayerHealth(Player p){
-		Damageable d = (Damageable) p;
-		return (int) d.getHealth();
+		return (int) ((Damageable) p).getHealth();
 	}
 
 	protected int getMaxPlayerHealth(Player p){
-		Damageable d = (Damageable) p;
-		return (int) d.getMaxHealth();
+		return (int) ((Damageable) p).getMaxHealth();
 	}
 
 	protected int getPlayerMissingHearts(Player p){
-		Damageable d = (Damageable) p;
-		return (int) d.getMaxHealth();
+		return (int) ((Damageable) p).getMaxHealth();
 	}
 
 	protected int getPlayerHealthPercentage(Player p){
 		int health = getPlayerHealth(p);
 		int maxhealth = getMaxPlayerHealth(p);
-		float percentage = (health*100)/maxhealth;
-		return (Math.round(percentage));
+		return (Math.round((health * 100) / maxhealth));
 	}
 
 	protected void disablePlayerTint(Player p){
-		String pname = p.getName();
-		togglelist.add(pname);
+		togglelist.add(p.getName());
 	}
 
 	protected void enablePlayerTint(Player p){
 		String pname = p.getName();
-		if (togglelist.contains(pname))
-			togglelist.remove(pname);
+		if (togglelist.contains(pname)) togglelist.remove(pname);
 	}
 
 	protected void togglePlayerTint(Player p){
-		if (isTintEnabled(p))
-			disablePlayerTint(p);
-		else
-			enablePlayerTint(p);
+		if (isTintEnabled(p)) disablePlayerTint(p);
+		else enablePlayerTint(p);
 	}
 
-	protected boolean isTintEnabled(Player p){
-		String pname = p.getName();
-		return !togglelist.contains(pname);
+	protected boolean isTintEnabled(Player p) {
+		return !togglelist.contains(p.getName());
 	}
 }
